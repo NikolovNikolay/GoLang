5dbcfd860bdd0acde2c051eb9b47f36c9d294e08
nikolay.nikolov <nnikolov@us.ibm.com>
2017-05-25 16:42:10 +0300

    Add first task from GoLang bootcamp book - Organize names

---
+++ GoLang_Bootcamp_ex/01.organize_names.go
@@ -0,0 +1,50 @@
+/**
+Given a list of names, you need to organize each name within a slice based on its length.
+
+Expected output:
+[[] [] [Ava Mia] [Evan Neil Adam Matt Emma] [Emily Chloe]
+[Martin Olivia Sophia Alexis] [Katrina Madison Abigail Addison Natalie]
+[Isabella Samantha] [Elizabeth]]
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+var names = []string{
+	"Katrina", "Evan", "Neil", "Adam", "Martin", "Matt",
+	"Emma", "Isabella", "Emily", "Madison",
+	"Ava", "Olivia", "Sophia", "Abigail",
+	"Elizabeth", "Chloe", "Samantha",
+	"Addison", "Natalie", "Mia", "Alexis"}
+
+var nameMap = make(map[int][]string)
+
+func main() {
+
+	fillNameMap(names, nameMap)
+	printMapVals(nameMap)
+}
+
+func fillNameMap(namesArr []string, mapParam map[int][]string) {
+	for i := range names {
+
+		n := names[i]
+		l := len(n)
+
+		if mapParam[l] == nil {
+			newSlice := []string{n}
+			mapParam[l] = newSlice
+		} else {
+			mapParam[l] = append(mapParam[l], n)
+		}
+	}
+}
+
+func printMapVals(mapParam map[int][]string) {
+	for i := range mapParam {
+		fmt.Printf("%q", mapParam[i])
+	}
+}
